739924ee2f19548e1a637e006136caaa62c103a7
Valeriya Gileva <62500965+gileva-vd@users.noreply.github.com>
2023-01-15 19:54:41 +0300

    Initial Commit

---
+++ ColorSliderSwiftUI/ColorSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderSwiftUIApp.swift
+//  ColorSliderSwiftUI
+//
+//  Created by Валерия Гилева on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
